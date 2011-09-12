ac69d97162c1377681b19fd398b51a49c8236d32
Zhengzheng Ye & Todd Ritchie <todd.ritchie@xtremelabs.com>
2011-09-12 16:19:39 -0400

    initial commit

---
+++ BootCamp-iOS/BootCamp_iOSAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  BootCamp_iOSAppDelegate.h
+//  BootCamp-iOS
+//
+//  Created by DX061 on 11-09-12.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BootCamp_iOSAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
